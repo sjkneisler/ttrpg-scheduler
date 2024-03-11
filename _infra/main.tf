a47766b633c557e854f11e2a8ede3ef06e97d634
sjkneisler <sjkneisler@sep.com>
2024-03-11 12:17:42 -0400

    Set desired ecs service count to 1

--- _infra/main.tf
+++ _infra/main.tf
@@ -140,7 +140,7 @@ resource "aws_ecs_service" "app_service" {
   cluster         = aws_ecs_cluster.app_ecs_cluster.id   # Reference the created Cluster
   task_definition = aws_ecs_task_definition.app_task.arn # Reference the task that the service will spin up
   launch_type     = "FARGATE"
-  desired_count   = 3 # Set up the number of containers to 3
+  desired_count   = 1
 
   load_balancer {
     target_group_arn = aws_lb_target_group.target_group.arn # Reference the target group
